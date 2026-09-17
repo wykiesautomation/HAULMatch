6bc8486f81a2f6c661006eff68e2042d686a1001
wykiesautomation <wykiesautomation@gmail.com>
2026-09-17 21:49:43 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.HAULMATCH_CONFIG={appsScriptUrl:"https://script.google.com/macros/s/AKfycbw0vLRtWJ_jDWLanasdrc7DLlog-QlGDTPMOmqMmH1Q5igcubRHYb-mIoUqnwmxbZNU/exec",supportEmail:"support@wykiesautomation.co.za",turnstileSiteKey:""};
+window.HAULMATCH_CONFIG={appsScriptUrl:"https://script.google.com/macros/s/AKfycbwqlzBjVYWyk7LQVczAEt9Oa2v6MnMQ-LbbtZuqPmJ6sjjqIEracGHbOkDRBFoaweWo/exec",supportEmail:"support@wykiesautomation.co.za",turnstileSiteKey:""};
