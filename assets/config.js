cac6e53f9f4446d2cb99fa6cd751e411cc5e51a3
wykiesautomation <wykiesautomation@gmail.com>
2026-09-20 17:54:18 +0200

    Update GOOGLE_CLIENT_ID in config.js

--- assets/config.js
+++ assets/config.js
@@ -9,7 +9,8 @@ window.HAULMATCH_CONFIG={
 window.HAULMATCH_CONFIG = window.HAULMATCH_CONFIG || {};
 
 window.HAULMATCH_CONFIG.GOOGLE_CLIENT_ID =
-  'PLAK_JOU_GOOGLE_WEB_CLIENT_ID_HIER';
+  '291364861368-9d7r04onl5e23jfn68e6b7rd3v0kkctd.apps.googleusercontent.com';
 
 window.HAULMATCH_CONFIG.APPS_SCRIPT_URL =
   'https://script.google.com/macros/s/AKfycbwqlzBjVYWyk7LQVczAEt9Oa2v6MnMQ-LbbtZuqPmJ6sjjqIEracGHbOkDRBFoaweWo/exec';
+
